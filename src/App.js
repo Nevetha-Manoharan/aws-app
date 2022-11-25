60981120894e2e6d3021d1be11f639312157f600
Nevetha Manoharan <nevetha_manoharan@epam.com>
2022-11-25 11:29:47 +0530

    changes pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import education_logo from "./cch_edu_Logo.png";
+import staffing_logo from "./cch-medical-network.jpg";
+import allied_logo from "./cch-allied-logo.png";
+import { Checklist } from "./components/checklistSection";
 
 function App() {
+  const options = [0, 1, 2, 3, 4, 5];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-holder">
+        <header className="content-header">
+          <div className="image-holder">
+            <img src={staffing_logo} alt="staffing_logo" className="logo" />
+            <img src={allied_logo} alt="allied_logo" className="logo" />
+            <img src={education_logo} alt="education_logo" className="logo" />
+          </div>
+          <div style={{ display: "flex", marginTop: "3%" }}>
+            <div style={{ flex: 1 }}>
+              <p>Experience: </p>
+              <div style={{ marginLeft: "20%" }}>
+                <p>[0] Not Appreciate</p>
+                <p>[1] No Experience</p>
+                <p>[2] Some Experience</p>
+                <p>[3] Intermittent Experience</p>
+                <p>[4] Experienced</p>
+                <p>[5] Very Experienced</p>
+              </div>
+            </div>
+            <div>
+              <p>Adjunct Nursing Facility</p>
+            </div>
+          </div>
+        </header>
+        <hr />
+        <div className="checklist-section">
+          <div className="col left">
+            <Checklist title="Assessment/Patient Care" options={options} />
+          </div>
+          <div className="col right">
+            <Checklist
+              title="Assessment/Patient Care Continued"
+              options={options}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const Data = [
+    {
+        title:'General',
+        requirements:[
+            "Adult Learning technique",
+            "classroom instructions",
+            "clinical instructions"
+        ]
+    },
+    {
+        title: 'General Clinical',
+        requirements:[
+            "Admission",
+            "Advance Directives",
+            "COllect Appropriate Data",
+            "Computerized Documentation",
+            "Discharge Teaching",
+            "Organ Tissue Donation",
+            "Patient and Family Teaching",
+            "Preoperative Teaching"
+        ]
+    },
+    {
+        title: 'Cardiovascular General',
+        requirements:[
+            "Abnormal heart sounds murmurs",
+            "Auscultation",
+            "Pulses/Circulation checks",
+            "Capillary Refill"
+        ]
+    },
+    {
+        title: 'Patient Experience',
+        requirements:[
+            "Abdominal Acritic",
+            "Acute Mi",
+            "Angina",
+            "Cariac Arrest/CPR",
+            "Cardio Myopathy",
+            "Pacemaker",
+            "Hypertension",
+            "Pericarditis"
+        ]
+    },
+]
